0254b8d8a6748dcbe7ab1e8441a77440b1dc4c86
Bruna de Carvalho <brunacarvalho260697@gmail.com>
2023-02-27 20:18:58 -0300

    FEAT: Queries das tabelas

---
+++ queries.sql
@@ -0,0 +1,24 @@
+CREATE TABLE User_Cookenu (
+	id VARCHAR(255) PRIMARY KEY, 
+    name VARCHAR(255) NULL, 
+    email VARCHAR(255) UNIQUE NOT NULL,
+    password VARCHAR(255) NOT NULL
+);
+
+CREATE TABLE Receitas_Cookenu (
+	id VARCHAR(255) PRIMARY KEY, 
+    title VARCHAR(255) NOT NULL, 
+    description TEXT NOT NULL,
+    creation_date TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    author_id varchar(255) NOT NULL,
+    FOREIGN KEY (author_id) REFERENCES User_Cookenu(id)
+);
+
+CREATE TABLE Follow_Cookenu (
+	id VARCHAR(255) PRIMARY KEY, 
+    id_follow VARCHAR(255) NOT NULL,
+    FOREIGN KEY (id_follow) REFERENCES User_Cookenu(id),
+    my_id VARCHAR(255) NOT NULL,
+    FOREIGN KEY (my_id) REFERENCES User_Cookenu(id)
+
+);
